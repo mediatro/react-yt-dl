afbcee799bf20192e7c1b0023b5b04a9354b7aa7
Mediatro <mediatro.dev@gmail.com>
2022-01-11 19:29:48 +0300

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { render } from 'react-dom'
+import { Box, ChakraProvider } from '@chakra-ui/react'
+//import { Titlebar, Color } from 'custom-electron-titlebar'
+import { Scrollbars } from 'react-custom-scrollbars'
 
-function App() {
+import theme from './styles/theme'
+import icon from './assets/icon.svg'
+
+import motion from 'framer-motion';
+
+import DownloadWatcher from './components/DownloadWatcher'
+import CloseModal from './components/CloseModal'
+
+import { DownloaderProvider } from './contexts/download'
+import { VideosProvider } from './contexts/videos'
+import { DownloadModalProvider } from './contexts/downloadModal'
+
+import Routes from './screens/Routes'
+
+import 'focus-visible/dist/focus-visible'
+import {BackendContextProvider} from "./contexts/backend";
+
+/*new Titlebar({
+  backgroundColor: Color.fromHex(theme.colors.gray[800]),
+  titleHorizontalAlignment: 'left',
+  menu: null,
+  icon
+})*/
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ChakraProvider theme={theme}>
+      <DownloaderProvider>
+        <VideosProvider>
+          <BackendContextProvider>
+            {/*<Routes />*/}
+          <CloseModal>
+            <DownloadModalProvider>
+              <Scrollbars
+                id="scroll-container"
+                style={{height: '100vh'}}
+                renderThumbVertical={(props) => (
+                  <Box
+                    {...props}
+                    opacity="0.7"
+                    bgColor="red.500"
+                    borderRadius="8px"
+                    zIndex="10"
+                  />
+                )}
+                renderView={(props) => (
+                  <Box
+                    minH="100%"
+                    background="gray.900"
+                    color="gray.100"
+                    overflowX="hidden"
+                    {...props}
+                  />
+                )}
+              >
+                <Routes />
+                <DownloadWatcher />
+              </Scrollbars>
+            </DownloadModalProvider>
+          </CloseModal>
+          </BackendContextProvider>
+        </VideosProvider>
+      </DownloaderProvider>
+    </ChakraProvider>
+  )
 }
 
 export default App;
+//render(<App />, document.getElementById('root'))
